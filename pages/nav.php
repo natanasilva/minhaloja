e843f1eedb7d0f0c23809ac1b091417a632934f0
Nataniel de Aguiar da Silva <nataniel.natanaguiar@gmail.com>
2023-04-03 20:41:45 -0300

    Meu primeiro commit

--- pages/nav.php
+++ pages/nav.php
@@ -10,32 +10,26 @@
                     <a class="me-3 py-2 text-dark text-decoration-none" href="index.php">Início</a>
                     <a class="me-3 py-2 text-dark text-decoration-none" href="produtos.php">Produtos</a>
                     <a class="me-3 py-2 text-dark text-decoration-none" href="fornecedores.php">Fornecedores</a>
-                    <a class="me-3 py-2 text-dark text-decoration-none" href="contato.php">Contate-nos</a>
-                    <?php
-                    if (!isset($_SESSION['usuarioNome'])) {
-                        echo "<a class='me-3 py-2 text-dark text-decoration-none' href='logar.php'>Minha Conta</a>";
-                    } else {
-                        echo "<ul class='navbar-nav me-auto mb-2 mb-lg-0'>
-                        <li class='nav-item dropdown'>
-                            <a class='nav-link dropdown-toggle' href='#' role='button' data-bs-toggle='dropdown' aria-expanded='false'>
-                                Minha Conta
-                            </a>
-                            <ul class='dropdown-menu'>
-                                <li><a class='dropdown-item' href='#'>Meu Perfil</a></li>
-                                <li><a class='dropdown-item' href='#'>Meu Pedidos</a></li>
-                                <li>
-                                    <hr class='dropdown-divider'>
-                                </li>
-                                <li><a class='dropdown-item' href='sair.php'>Sair</a></li>
-                            </ul>
-                        </li>
-                    </ul>";
-                    }
-                    ?>
+                    <?php if (!isset($_SESSION['usuarioNome'])) : ?>
+                        <a class="me-3 py-2 text-dark text-decoration-none" href="logar.php">Minha Conta</a>
+                    <?php else : ?>
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Minha Conta
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Meu Perfil</a></li>
+                                    <li><a class="dropdown-item" href="#">Meu Pedidos</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="sair.php">Sair</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    <?php endif ?>
+
                 </nav>
-            </div>
-            <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
-                <h1 class="display-4 fw-normal">Os Melhores Produtos</h1>
-                <p class="fs-5 text-muted">Bem vindo(a) a nossa loja. Temos os melhores produtos e os melhores preços pra você!!!</p>
             </div>
         </header>
